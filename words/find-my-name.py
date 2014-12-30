703477187b5e1a1ac93b055738598fcc5442a845
Andrew Hurst <andrew@hurstdog.org>
2014-12-30 10:45:09 -0800

    Add a program to search for names in dict.

---
+++ words/find-my-name.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python
+
+# A simple program to find my kids names in the dictionary to show them a
+# little programming.
+
+_WORDS = '/usr/share/dict/words'
+
+def getWords():
+  print 'Getting words from {0}...'.format(_WORDS)
+  f = open(_WORDS)
+  lines = f.readlines()
+  print 'I found {0} words.'.format(len(lines))
+  return lines
+
+def findByName(words, name):
+  print '---- Searching for {0}...'.format(name)
+  for word in words:
+    smallword = word.lower()
+    if name.lower() in smallword:
+      print '{0} is in {1}!'.format(name, word.strip())
+
+def findMyWords():
+  print 'Hi Blake, Reade, and Piper!'
+  words = getWords()
+  findByName(words, 'Reade')
+  findByName(words, 'Blake')
+  findByName(words, 'Piper')
+
+if __name__ == '__main__':
+  findMyWords()
